aff50a6045c6fce9112876cdcd537194b9e266e4
Citl-Dev-Ops <panderson@rtc.edu>
2025-07-03 16:49:16 -0700

    Create fetchSchedule.js

---
+++ api/fetchSchedule.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
+
+  try {
+    const response = await fetch('https://classes.sbctc.edu/fetchSchedule', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(req.body)
+    });
+
+    if (!response.ok) {
+      return res.status(response.status).json({ error: 'SBCTC API error' });
+    }
+
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.status(500).json({ error: 'Server error', details: err.message });
+  }
+}
