20838bcb921246e1247672d65ccd4598a92ac015
Citl-Dev-Ops <panderson@rtc.edu>
2025-07-07 18:16:59 -0700

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/rtc-openapi/', // match GitHub repo path if using GitHub Pages
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
+  }
 });
