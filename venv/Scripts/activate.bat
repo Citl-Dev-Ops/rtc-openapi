7d4d6f5e70b422cdc2118bc3ce4c8ccb2bd96517
CITL-DevOps <armcdowell@rtc.edu>
2026-03-31 05:45:04 -0700

    HENOSIS sync upload [HENOSIS_CODE__RTC_OPENAPI] 2026-03-31 05:45:01

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\armcdowell\OneDrive - Renton Technical College\Documents\GitHub\rtc-openapi\venv
+set VIRTUAL_ENV=C:\00 HENOSIS CODING PROJECTS\CITL PROJECTS\rtc-openapi\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
